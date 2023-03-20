a4b72d7f02b9fa23abb2321bb081d806b196eb96
rinjpapp <iamryoinjp@gmail.com>
2023-03-20 11:09:31 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status
+
+  with_option presence: true do
+    validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :nickname
+  end
 end
