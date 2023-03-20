54981f0c093db86882540d1aff618c3daedd148f
rinjpapp <iamryoinjp@gmail.com>
2023-03-20 12:20:43 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  devise_scope :user do
+    get 'individuals', to: 'users/registrations#new_individual'
+    post 'individuals', to: 'users/registrations#create_individual'
+  end
   root to: 'homes#index'
 end
