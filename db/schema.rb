114d477e5b887851bf41eb02238f57a36d56ff63
rinjpapp <iamryoinjp@gmail.com>
2023-03-20 12:00:59 +0900

    individulalモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_20_015447) do
+ActiveRecord::Schema.define(version: 2023_03_20_025435) do
+
+  create_table "individuals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birth_day", null: false
+    t.integer "phone_id", null: false
+    t.string "phone_number", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_individuals_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "status_id", null: false
@@ -26,4 +40,5 @@ ActiveRecord::Schema.define(version: 2023_03_20_015447) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "individuals", "users"
 end
