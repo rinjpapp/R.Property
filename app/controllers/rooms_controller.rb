9f84ae0a730c5dad86086d04a8c1bdf85ca35e89
rinjpapp <iamryoinjp@gmail.com>
2023-03-22 19:56:25 +0900

    URLからの直接アクセスを拒否

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,21 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_admin!
+
   def new
     
+  end
+
+  private
+
+  def authenticate_admin!
+    if user_signed_in?
+      if current_user.status_id != 2 && current_user.status_id ==3 
+        redirect_to applicants_path
+      elsif current_user.status_id != 3 && current_user.status_id == 2
+        redirect_to residents_path
+      end
+    else
+      redirect_to root_path
+    end
   end
 end
