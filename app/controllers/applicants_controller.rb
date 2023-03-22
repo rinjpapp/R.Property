4b07a79c5434b0306dc0fc176573b545e19f540b
rinjpapp <iamryoinjp@gmail.com>
2023-03-22 20:04:26 +0900

    コントローラ修正

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,15 +1,18 @@
 class ApplicantsController < ApplicationController
   before_action :authenticate_user!
-  before_action :user_only_applicant
 
   def index
   end
 
   private 
 
-  def user_only_applicant
-    if current_user.status_id != 3 && current_user.status_id == 2
-      redirect_to residents_path
+  def authenticate_user!
+    if user_signed_in?
+      if current_user.status_id != 3 && current_user.status_id == 2
+        redirect_to residents_path
+      end
+    else
+      redirect_to root_path
     end
   end
 end
--- app/controllers/residents_controller.rb
+++ app/controllers/residents_controller.rb
@@ -1,15 +1,18 @@
 class ResidentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :user_only_resident
 
   def index
   end
 
   private
 
-  def user_only_resident
-    if current_user.status_id != 2 && current_user.status_id ==3
-      redirect_to applicants_path
+  def authenticate_user!
+    if user_signed_in?
+      if current_user.status_id != 2 && current_user.status_id ==3
+        redirect_to applicants_path
+      end
+    else
+      redirect_to root_path
     end
   end
 end
