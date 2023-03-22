9465ba3102c5478d2985d1249462d8f921cac1c1
rinjpapp <iamryoinjp@gmail.com>
2023-03-22 17:49:21 +0900

    アソシエーションとバリデーション

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,15 @@
 class Room < ApplicationRecord
+  belongs_to :admin
+  belongs_to :building
+
+  with_options presence: true do
+    validates :room_number
+    validates :rent
+    validates :management_fee
+    validates :deposit
+    validates :key_money
+    validates :layout
+    validates :floor_area
+    validates :available_date
+  end
 end
