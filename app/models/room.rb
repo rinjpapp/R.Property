74c3e052fedf09e98a455de21e7b0c7fd5564c74
rinjpapp <iamryoinjp@gmail.com>
2023-03-23 15:11:04 +0900

    validation修正

--- app/models/room.rb
+++ app/models/room.rb
@@ -10,7 +10,8 @@ class Room < ApplicationRecord
     validates :deposit, numericality: { only_integer: true }
     validates :key_money, numericality: { only_integer: true }
     validates :layout
-    validates :floor_area, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999.9, message: "must be a number with one decimal place or less", only_integer: true }
+    validates :floor_area, format: { with: /\A\d+(\.\d{1})?\z/, message: "must be a number with one decimal place or less" }
     validates :available_date
+    validates :image
   end
 end
