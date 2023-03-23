0f9cd7eee38711b68aa13f4d1ed08ddb4b8e82b2
rinjpapp <iamryoinjp@gmail.com>
2023-03-23 10:56:09 +0900

    コントローラー修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,8 @@
 class RoomsController < ApplicationController
-  before_action :authenticate_admin!
+  before_action :access_only_admin
 
   def new
-    
+    @room = Room.new
   end
 
   def create
@@ -11,15 +11,19 @@ class RoomsController < ApplicationController
 
   private
 
-  def authenticate_admin!
-    if user_signed_in?
-      if current_user.status_id != 2 && current_user.status_id ==3 
-        redirect_to applicants_path
-      elsif current_user.status_id != 3 && current_user.status_id == 2
-        redirect_to residents_path
-      end
+  def access_only_admin
+    if admin_signed_in?
+      authenticate_admin!
     else
-      redirect_to root_path
+      if user_signed_in?
+        if current_user.status_id != 2 && current_user.status_id ==3 
+          redirect_to applicants_path
+        elsif current_user.status_id != 3 && current_user.status_id == 2
+          redirect_to residents_path
+        end
+      else
+        redirect_to root_path
+      end
     end
   end
 end
