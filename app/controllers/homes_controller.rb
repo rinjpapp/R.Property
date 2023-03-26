e11b6594d9e3e722bb0f16ab5ce2101930202fc3
rinjpapp <iamryoinjp@gmail.com>
2023-03-26 20:03:01 +0900

    searchアクション定義

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,13 @@
 class HomesController < ApplicationController
+
   def index
+  end
+
+  def show
+  end
+
+  def search
+    @q = Room.includes(:building).ransack(params[:q])
+    @rooms = @q.result
   end
 end
