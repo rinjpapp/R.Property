298745702d63f9b2cb4b20eb299847bb8e7706f9
rinjpapp <iamryoinjp@gmail.com>
2023-04-07 14:11:27 +0900

    ビュー作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,7 @@
 class RoomsController < ApplicationController
   before_action :access_only_admin
-  before_action :set_building
+  before_action :set_building, only: [:new, :create]
+  before_action :set_ransack, only: :show
 
   def new
     @room = Room.new
@@ -15,6 +16,10 @@ class RoomsController < ApplicationController
       flash.now[:danger] = "登録に失敗しました"
       render :new
     end
+  end
+
+  def show
+    @room = Room.find(params[:id])
   end
 
   private
@@ -42,5 +47,14 @@ class RoomsController < ApplicationController
 
   def set_building
     @building = Building.find_by(id: params[:room])
+  end
+
+    def set_ransack
+    if params[:q]&.dig(:layout)
+      squished_keywords = params[:q][:layout].squish
+      params[:q][:layout_cont_any] = squished_keywords.split(" ")
+    end
+    @q = Room.includes(:building).ransack(params[:q])
+    @rooms = @q.result
   end
 end
